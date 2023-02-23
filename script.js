7e55c328f3036931da67ac270e09b1377bb8c46d
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-02-23 14:44:08 -0600

    functions

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-
-let userName = prompt("Hello! What is your name?");
-console.log(userName);
-document.write("Hello, " + userName);
-
-let favAnimal = prompt("Hello, " + userName + " which of these three animals do you like the most? Bush babies, owls, or dogs");
-console.log(favAnimal)
-if(favAnimal == "dogs"){
-    document.write(" They are fascinating, did you know");
+function greeting(){
+    let userName = prompt("Hello! What is your name?");
+    console.log(userName);
+    document.write("Hello, " + userName);
+    return document.write();
 }
-if(favAnimal == "owls"){
-    document.write(" They are fascinating, did you know");
-}
-if(favAnimal == "bush babies"){
-    document.write(" They are fascinating, did you know");
-}
-else document.write(" you do not follow directions... ");
 
 
-if(favAnimal == "dogs"){
-    document.write(" dogs were bread for different reason all coming from wolves originally!");
-}
-else if(favAnimal == "bush babies"){
-    document.write(" bush babies jump from tree to tree making it look like they are able to fly!");
-}
-else if(favAnimal == "owls") {
-    document.write(" they are able to rotate their head 270 degrees!");
-}
-else document.write("You do not like animals?");
 
-let moreInfo = prompt("Wanna know a secret about this page? yes or no");
-if(moreInfo == "yes"){
-    document.write(" Click the pictures!");
+function animalChoice(){
+    let favAnimal = prompt("Which of these three animals do you like the most? Bush babies, owls, or dogs");
+    console.log(favAnimal);
+    if(favAnimal == "dogs"){
+        document.write(" They are fascinating, did you know dogs were bread for different reason all coming from wolves originally!");
+    }
+    else if(favAnimal == "owls"){
+        document.write(" They are fascinating, did you know they are able to rotate their head 270 degrees!");
+    }
+    else if(favAnimal == "bush babies"){
+        document.write(" They are fascinating, did you know bush babies jump from tree to tree making it look like they are able to fly!");
+    }
+    else fix = prompt("Error " + favAnimal + " was not one of the options!");
+    console.log(fix)
+    if(fix == "dogs"){
+        document.write(" They are fascinating, did you know dogs were bread for different reason all coming from wolves originally!");
+    }
+    else if(fix == "owls"){
+        document.write(" They are fascinating, did you know they are able to rotate their head 270 degrees!");
+    }
+    else if(fix == "bush babies"){
+        document.write(" They are fascinating, did you know bush babies jump from tree to tree making it look like they are able to fly!");
+    }
+    else document.write(" You really do not follow instructions...")
+    return document.write();
 }
-else document.write(" Fine I won't tell you...");
+
+
+
+function secretLinks(){
+    let moreInfo = prompt("Wanna know a secret about this page? yes or no");
+    console.log(moreInfo);
+    if(moreInfo == "yes"){
+        document.write(" Click the pictures!");
+    }
+    else document.write(" Fine I won't tell you...");
+    return document.write();
+}
